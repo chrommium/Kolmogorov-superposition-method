4d18aea99801aeb3577931ecf6946737c2ecb859
Roman.Sorokin <rsorokin@neoflex.ru>
2021-03-17 14:28:47 +0300

    [w.i.p.]: test numerical functions

--- Poisson.py
+++ Poisson.py
@@ -5,26 +5,80 @@ import matplotlib.pyplot as plt
 alpha1 = 0.5
 alpha2 = 0.5
 
+# tested
 def psi_from_y(x, z, gen):
     return (z/alpha2) - (alpha1/alpha2)*gen.interpolated_psi_k(x)
 
+# tested
 def y(x, z, gen):
     return gen.backward_psi_k(psi_from_y(x, z, gen))
 
+# tested
 def r(x, z, gen):
     return alpha2*gen.interpolated_psi_k_prime(y(x,z, gen))
 
-def g_wo_phi(x,z,gen):
-    numerator = (alpha1*gen.interpolated_psi_k_prime(x))**2 + (alpha2*gen.interpolated_psi_k_prime(y(x,z,gen)))**2
-    return numerator / r(x,z,gen)
+# tested
+def r_prime(x, z, gen):
+    return alpha2*gen.interpolated_psi_k_prime2(y(x,z, gen)) / r(x,z,gen)
 
+# tested
+def n_func(x, z, gen):
+    return (alpha1*gen.interpolated_psi_k_prime(x))**2 + (alpha2*gen.interpolated_psi_k_prime(y(x,z,gen)))**2
+
+# tested
+def n_prime(x, z, gen):
+    factor1 = 2*(alpha2**2)*gen.interpolated_psi_k_prime(y(x,z,gen))
+    factor2 = gen.interpolated_psi_k_prime2(y(x,z,gen)) / r(x,z,gen)
+    return factor1 * factor2 
+
+def g_wo_phi(x, z, gen):
+    return n_func(x,z,gen) / r(x,z,gen)
+
+# tested
 def g_wo_phi_prime(x,z,gen):
-    first_term_numerator = 2 * alpha2**2 * gen.interpolated_psi_k_prime(y(x,z,gen))
-    second_term_numerator = g_wo_phi(x,z,gen) / r(x,z,gen)
-    return (first_term_numerator + alpha2 * second_term_numerator / r(x,z,gen)) * gen.interpolated_psi_k_prime2(y(x,z,gen))
+    first_term = n_prime(x,z,gen)
+    second_term = n_func(x,z,gen) * r_prime(x,z,gen) / r(x,z,gen)
+    return (first_term - second_term) / r(x,z,gen)
 
 def f_func(x,z, gen):
     return math.sin(math.pi*x) * math.sin(math.pi*y(x,z,gen))
+
+# tested
+def I_from_func(f, x_min, x_max, step):
+    if x_max - x_min < step:
+        return (x_max - x_min) * (f(x_min) + f(x_max)) / 2
+    N = math.ceil((x_max - x_min) / step)
+    step = (x_max - x_min) / N
+    res = f(x_min) * step / 2
+    for i in range(1, N):
+        res += f(x_min+i*step) * step
+    res += f(x_max) * step / 2
+    return res
+
+# arr = [i for i in range(100)]
+# res = lambda x: I_from_func((lambda t: t**2), 0, x, 0.001)
+# res_arr = [res(i*0.05) for i in arr]
+# correct_res = lambda x: x**3/3
+# correct_res_arr = [correct_res(i*0.05) for i in arr]
+# plt.plot(arr, res_arr)
+# plt.plot(arr, correct_res_arr)
+# plt.legend()
+# plt.show()
+
+def x_low(z):
+    if z < alpha2:
+        return 0
+    return gen.backward_psi_k((z-alpha2)/alpha1)
+
+def x_high(z):
+    if z < alpha1:
+        return gen.backward_psi_k(z/alpha1)
+    return 1
+
+def I_g_wo_phi(z):
+    f = lambda x: g_wo_phi(x,z,gen)
+    return I_from_func(f, x_low(z), x_high(z), gen.g**(-gen.k))
+
 
 # def generate_phi_from_y(x, gen):
 #     return lambda z: psi_from_y(x,z,gen)
@@ -41,11 +95,21 @@ def diff(f, step):
     return f_prime
 
 # z_array = [i*10**-7 for i in range(274000,276000)]
-z_array = [i*10**-6 for i in range(27000,28000)]
+z_array = [i*5*10**-7 for i in range(2*27000,2*28000)]
 y_array = [y(0.25, i, gen) for i in z_array]
 y_prime = diff(lambda z: y(0.25, z, gen), 10**-7)
-r_array = [1/r(0.25, i, gen) for i in z_array]
 y_prime_array = [y_prime(i) for i in z_array]
+r_array = [1/r(0.25, i, gen) for i in z_array]
+
+# n_prime_array = [n_prime(0.25, i, gen) for i in z_array]
+# n_prime_num = diff(lambda z: n_func(0.25, z, gen), 10**-7)
+# n_prime_array_num = [n_prime_num(i) for i in z_array]
+# plt.plot(z_array, n_prime_array)
+# plt.plot(z_array, n_prime_array_num)
+# plt.legend()
+# plt.show()
+
+
 g_wo_phi_array = [g_wo_phi(0.25, i, gen) for i in z_array]
 g_wo_phi_prime_array = [g_wo_phi_prime(0.25, i, gen) for i in z_array]
 
@@ -65,6 +129,7 @@ g_wo_phi_prime_num_array = [g_wo_phi_prime_num(i) for i in z_array]
 # plt.plot(z_array, g_wo_phi_array)
 # plt.legend()
 # plt.show()
+
 plt.plot(z_array, g_wo_phi_prime_array)
 # plt.legend()
 # plt.show()
